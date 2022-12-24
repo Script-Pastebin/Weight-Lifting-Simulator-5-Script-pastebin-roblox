7281408de420770f4423b7925f6b3d6cec68b0b3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:12 +0100

    commit zero

---
+++ Weight Lifting Simulator 5 Script pastebin roblox Weight Lifting Simulator 5 .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/PydXZGXY"))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+Weight lifting simulator 5 script. It has: auto farm, teleports, setwalkspeed and more!
+]]
